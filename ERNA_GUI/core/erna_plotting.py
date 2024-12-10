4e26e08bd10b001c2e46a4543d622bbd97df6437
Matteo Vissani <matteo.vissani92@gmail.com>
2024-12-10 15:37:18 -0500

    models tsv

--- ERNA_GUI/core/erna_plotting.py
+++ ERNA_GUI/core/erna_plotting.py
@@ -5,17 +5,31 @@ from abc import ABC
 class Plotting(ABC):
     
     def __init__(self, results):
-        self._results = results
-        self._data_cont = []
-        self._stim_amp = []
-        self._timebursts = []
-        self.
+        self._prepare_results(results)
         
         
-    def _unpack_results(self):
-        for data in self._results:
-            self._data_cont = data_run['data']['erna_cont']
         
+    def _prepare_results(self,results): 
+        self._run = results['run']
+        self._task = results['task']
+        self._rrun_details= results['run_details']
+        self._stimChannels = results['stimChannels']
+        self._time_gtc = results['time_gtc']
+        self._run = results['run']
+        self._run = results['run']
+        self._run = results['run']
+        self._run = results['run']
+        
+        data_run['run'].append(run)
+        data_run['task'].append(task)
+        data_run['run_details'].append(result.get('run_details', None))
+        data_run['stimChannels'].append(result.get('stimChannels', None))
+        data_run['time_gtc'].append(result['data'].get('time', None))
+        data_run['data_cont'].append(result['data'].get('erna_cont', None))
+        data_run['data_erna'].append(result['data'].get('erna_data', None))
+        data_run['timebursts'].append(result['data'].get('timebursts', None))
+        data_run['stim_amp'].append(result['data'].get('stim_amp', None))
+            
         
     @property
     def results(self):
@@ -24,6 +38,7 @@ class Plotting(ABC):
 
 
 def plot_single_run_results():
+    
     pass
 
 
